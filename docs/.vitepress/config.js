b8a765fa6718355a84f43fca5428998c33da06f3
andres-geotec <andres.geotec@gmail.com>
2023-06-30 16:07:21 -0600

    vitepress: custom theme

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "mapas-vue3",
+  description: "pruebas para la siguiente versión de mapas"
+})
