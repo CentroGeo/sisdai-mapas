4bf028f49cb73670b38ec7dd268de7ff77b8d010
andres-geotec <andres.geotec@gmail.com>
2023-09-19 10:07:16 -0600

    eliminando archivos no utilizados

--- src/main.js
+++
@@ -1,8 +0,0 @@
-// import App from '@/ejemplos/App.vue'
-import App from '@/ejemplos/VistaGema.vue'
-// import App from '@/ejemplos/DispocisionPaneles.vue'
-import { createApp } from 'vue'
-
-import '@/assets/main.css'
-
-createApp(App).mount('#app')
