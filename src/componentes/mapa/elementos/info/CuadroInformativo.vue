469ca90b3c77a91103dadd3ed6ea20b5ce831186
andres-geotec <andres.geotec@gmail.com>
2025-07-24 10:41:16 -0600

    🔧 feat: obtención de pinxel de una coordenada cuando se mueve el mapa

--- src/componentes/mapa/elementos/info/CuadroInformativo.vue
+++ src/componentes/mapa/elementos/info/CuadroInformativo.vue
@@ -1,16 +1,48 @@
 <script setup>
-import { inject } from 'vue'
+import { inject, ref } from 'vue'
 
-import PointerEventType from 'ol/pointer/EventType'
+import MapBrowserEventType from 'ol/MapBrowserEventType'
+// import EventType from 'ol/events/EventType'
+// import PointerEventType from 'ol/pointer/EventType'
 
 import { MAPA_INYECTADO } from './../../../../utiles/identificadores'
 
 const mapa = inject(MAPA_INYECTADO)
-// watch(() => mapa.getTargetElement(), alSalirCursor)
-mapa.on(PointerEventType.POINTERDOWN, alClick)
+const visible = ref(false)
+const coordenadas = ref()
+mapa.on(MapBrowserEventType.SINGLECLICK, alClick)
 function alClick({ dragging, originalEvent /*, map */ }) {
-  console.log('se dió click')
+  if (dragging || originalEvent.target.closest('.sisdai-mapa-control')) {
+    // console.log('Aquí no')
+    return
+  }
+
+  const pixel = mapa.getEventPixel(originalEvent)
+
+  visible.value = true
+
+  coordenadas.value = mapa.getCoordinateFromPixel(pixel)
+  // console.log(mapa.getPixelFromCoordinate(coordenadas))
 }
+
+// mapa.on(PointerEventType.POINTERMOVE, () => {
+// mapa.getView().on(EventType.CHANGE, () => { // funciona
+mapa.on(MapBrowserEventType.POINTERDRAG, () => { // funciona con el mause
+  // console.log('moviendo')
+
+  if (coordenadas.value === undefined) {
+    return
+  }
+
+  // console.log(mapa.getPixelFromCoordinate(coordenadas.value))
+})
+
+function cerrarCuadro() {
+  coordenadas.value = undefined
+  visible.value = false
+}
+
+const cargando = '<span class="pictograma-reloj pictograma-grande" />'
 </script>
 
 <template>
@@ -18,13 +50,22 @@ function alClick({ dragging, originalEvent /*, map */ }) {
     aria-hidden="true"
     aria-live="assertive"
     class="ol-unselectable globo-informacion cuadro-informacion-capa"
+    :class="{ oculto: !visible }"
     ref="sisdaiCuadroInfo"
     role="tooltip"
   >
     <div
       class="globo-informacion-cuerpo"
-      v-html="globo.contenido"
+      v-html="cargando"
     />
+    <button
+      aria-label="Cerrar"
+      class="globo-informacion-cerrar"
+      type="button"
+      @click="cerrarCuadro"
+    >
+      <span class="pictograma-cerrar" aria-hidden="true" />
+    </button>
   </div>
 </template>
 
