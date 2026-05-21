1a572600b0767a0ccf88def52e3eb6947c98348d
andres-geotec <andres.geotec@gmail.com>
2026-05-20 21:26:44 -0600

    fix(leyenda wms): elimina check no utilizado

--- docs/.vitepress/theme/ejemplos/CapaArcgis.vue
+++ docs/.vitepress/theme/ejemplos/CapaArcgis.vue
@@ -1,33 +1,8 @@
-<script setup>
-import { ref } from 'vue'
-
-const checkedNames = ref([])
-</script>
-
 <template>
   <SisdaiMapa
     descripcion="Mapa básico con una capa de servicio de Arcgis."
     :vista="{ extension: '-101.4,10.3,0,70.7', extensionMargen: '10,70,10,10' }"
   >
-    <template #panel-encabezado-vis>
-      <div>Checked names: {{ checkedNames }}</div>
-
-      <input
-        type="checkbox"
-        id="jack"
-        value="Jack"
-        v-model="checkedNames"
-      />
-      <label for="jack">Jack</label>
-
-      <input
-        type="checkbox"
-        id="john"
-        value="John"
-        v-model="checkedNames"
-      />
-      <label for="john">John</label>
-    </template>
     <SisdaiCapaXyz :posicion="1" />
 
     <SisdaiCapaArcgis
